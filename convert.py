448d4f344be2ee603c91657509101b43605f6455
eh1blum <31659394+eh1blum@users.noreply.github.com>
2017-12-07 02:31:13 -0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,3 +1,6 @@
+## Function that reads a txt file and writes the content of the file as an array in a js file ##
+## Helper Function ##
+
 with open('liberal_array.js', 'w') as outfile:
   with open('liberal_text.txt', 'r') as infile:
     outfile.write('let liberal_array = [\n')
